67877df124bd50d48a73271187f25a1cd20053b7
Chien Kuang Cheng <jason@kingsteel.com>
2017-01-23 16:33:28 +0800

    Abstract geocode API Add Forecast.io API

---
+++ geocode/geocode.js
@@ -0,0 +1,42 @@
+/**
+ * Created by KC on 23/01/2017.
+ */
+
+const request = require('request');
+
+
+
+var geocodeAddress = (address, callback) => {
+
+    // Encode string from user input
+    var encodedAddress = encodeURIComponent(address);
+
+    // Get geocode through GOOGLE MAPS Geocode API
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&lang=zh-TW`,
+        json: true
+    }, (error, response, body) => {
+        //console.log(JSON.stringify(response, undefined, 2));
+        //Handle callback error
+        if(error) {
+            callback('Unable to connect to Google servers.');
+        }
+        else if (body.status === 'ZERO_RESULTS') {
+            callback('Unable to find address.');
+        }
+        else if(response.statusCode !== 200) {
+            callback('Fetch error');
+        } else {
+
+            callback(undefined, {
+                address:body.results[0].formatted_address,
+                latitude:body.results[0].geometry.location.lat,
+                longitude:body.results[0].geometry.location.lng
+            });
+        }
+    });
+};
+
+module.exports.geocodeAddress = geocodeAddress;
+
+
